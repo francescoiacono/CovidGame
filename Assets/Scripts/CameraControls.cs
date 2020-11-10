3882b0babf0f2938cbf5d8e95bdd467a9350f40f
Francesco <sennozeta@gmail.com>
2020-11-10 14:51:45 +0000

    Prototype Started
    Added PlayerMovement, CameraControls and a basic models

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public Transform Player;
+    private float Height;
+    private float Dist;
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(Player.position.x, Player.position.y, transform.position.z);
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    private Rigidbody rigidBody;
+
+    public float rotationRate = 360;
+    public float movingSpeed = 10;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.centerOfMass = Vector3.zero;
+        rigidBody.inertiaTensorRotation = Quaternion.identity;
+    }
+
+    void FixedUpdate()
+    {
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float turnAxis = Input.GetAxis(turnInputAxis);
+
+        ApplyInput(moveAxis, turnAxis);
+    }
+
+
+    private void ApplyInput(float moveInput, float turnInput)
+    {
+        Move(moveInput);
+        Turn(turnInput);
+    }
+
+    private void Move(float input)
+    {
+        rigidBody.AddForce(transform.forward * input * movingSpeed, ForceMode.Force);
+    }
+
+    private void Turn(float input)
+    {
+        transform.Rotate(0, rotationRate * input * Time.deltaTime, 0);
+    }
+
+}
