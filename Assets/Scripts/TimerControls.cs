9334135a2de810381772a3258e870abefa3b1da2
Francesco <sennozeta@gmail.com>
2020-11-12 12:22:48 +0000

    Added a virus bar and a timer

---
+++ Assets/Scripts/TimerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerControls : MonoBehaviour
+{
+    public int MaxTime = 100;
+    public VirusBar virusBar;
+    private float currentTime;
+    private Text t;
+
+    private void Start()
+    {
+        currentTime = 0f;
+        virusBar.SetMaxInfectionRate(MaxTime);
+        t = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (currentTime <= MaxTime)
+        {
+            currentTime += Time.deltaTime;
+            int time = (int)currentTime;
+            virusBar.SetInfectionRate(time);
+            string textTime = time.ToString();
+            t.text = textTime;
+        }
+    }
+
+}
---
+++ Assets/Scripts/VirusBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirusBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void SetMaxInfectionRate(int maxInfection)
+    {
+        slider.maxValue = maxInfection;
+    }
+
+    public void SetInfectionRate(int infection)
+    {
+        slider.value = infection;
+    }
+
+
+
+}
